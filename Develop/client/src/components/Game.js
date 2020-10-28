a3f6dbec5744b77013ea8ac78dff97ddd81848de
Desert Python <mattsenneff@aol.com>
2020-10-28 12:44:14 -0700

    remained game comp

--- Develop/client/src/components/Game.js
+++ Develop/client/src/components/Game.js
@@ -6,5 +6,48 @@ const Game = () => {
     const [history , setHistory] = useState([9].fill(null));
     const [stepNumber , setStepNumber] = useState(0);
     const [xIsNext , setXisNext] = useState(true);
-    const winner = 0;
+    const winner = calculateWinner(history[stepNumber]);
+    const xO = xIsNext ? "X" : "O";
+
+    const handleClick = (i) => {
+        const historyPoint = history.slice(0, stepNumber +1);
+        const current = historyPoint[stepNumber];
+        const squares = [...current];
+        // return if won or occupied
+        if (winner || squares[i]) return;
+        //select the square
+        squares[i] =xO;
+        setHistory([...historyPoint, squares]);
+        setStepNumber(historyPoint.length);
+        setXisNext(!xIsNext);
+    };
+
+    const jumpTo = (step) => {
+        setStepNumber(step);
+        setXisNext(step % 2 === 0)
+    };
+    
+    const renderMoves = () => {
+        history.map((_step, move) => {
+            const destinaton = move ? `Go to move #${move}` : "Go to Start";
+            return (
+                <li key = {move}>
+                    <button onClick= {()=> jumpTo (move)}>{destinaton}</button>
+                </li>
+            )
+        })
+    }
+    return (
+        <>
+            <h1>React Tic Tac Toe with Hooks</h1>
+            <Board squares = {history[stepNumber]} onclick = {handleClick} />
+            <div className = "info-wrapper">
+                <div>
+                    <h3>History</h3>
+                    {renderMoves()}
+                </div>
+                <h3>{winner ? "Winner: " + winner : "Next Player: " + xO} </h3>
+            </div>
+        </>
+    )
 }
