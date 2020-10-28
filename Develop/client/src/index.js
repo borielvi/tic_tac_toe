d245058c406fa12929e5d8122222d1ae20989ae0
Desert Python <mattsenneff@aol.com>
2020-10-28 15:00:26 -0700

    edit

--- Develop/client/src/index.js
+++ Develop/client/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+//import css file
+import Game from "./components/Game"
+// import App from "./App"; replaced by Game
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+ReactDOM.render(<Game />, document.getElementById("root"));
+// registerServiceWorker();
