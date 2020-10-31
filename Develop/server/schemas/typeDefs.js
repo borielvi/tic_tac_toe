e0c3cb685c0c8786fe3f3d816f65b0fe4c0b0c00
borielvi <borielvi@isu.edu>
2020-10-31 16:05:54 -0700

    started on adding graphql

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,5 +1,38 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    type 
+    type User {
+        _id: ID
+        username: String
+        email: String
+        games: [Game]
+    }
+
+    type Game {
+        _id: ID
+        winner: String
+        loser: String
+        createdAt: String
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+        games(username: String): [Game]
+        game(_id: ID!): Game
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addGame(winner: String!, loser: String!)
+    }
 `
+
+module.exports = typeDefs;
